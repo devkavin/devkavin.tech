2307c75425c732ca345c94195d4f9de09a196ae2
devkavin <kavindra.senanayake@gmail.com>
2024-02-14 01:30:39 +0530

    website layout created and layout set

---
+++ components/main/StarBackground.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React, {useState, useRef, Suspense} from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import {Points, PointMaterial, Preload} from '@react-three/drei'
+// import * as random from 'maath/random/dist/maath-random.esm'
+
+export const StarBackground = () => {
+  return (
+    <div>StarBackground</div>
+  )
+}
